11cecaa133622a7400e7a9c3a8f53c13910d03e2
KondratievViktor <132542952+KondratievViktor@users.noreply.github.com>
2023-05-31 22:38:29 +0300

    Add files via upload

---
+++ ItTask.bat
@@ -0,0 +1,19 @@
+echo off
+md Kondratiev
+cd Kondratiev
+md Viktor
+md Evgenevich
+echo > 04051999.txt
+cd Evgenevich
+echo > SvoyNout.txt
+pause
+cd ..
+cd ..
+del Kondratiev /S/Q/F
+pause
+cd Kondratiev
+rd Viktor
+rd Evgenevich
+cd..
+rd Kondratiev
+pause
